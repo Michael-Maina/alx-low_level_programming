1e8685582bb8be69d66d8cbbfffbc19ca51a90d5
rog-pog <michaelmaina02@gmail.com>
2022-09-08 03:43:51 -0700

    4-puts.c added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/* Prints string */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+	// initializing string
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+
+	//writing to stdout
+	puts(str1);
+
+	return 0;
+
+}
+	
