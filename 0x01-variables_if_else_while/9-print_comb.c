107a44338a8901a99ae5296b0be50aa36690ffd7
rog-pog <michaelmaina02@gmail.com>
2022-09-09 12:27:43 -0700

    9-print_comb.c added

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,16 @@ int main(void)
 	int i;
 
 	for (i = 48; i <= 57; i++)
+	{
 		putchar(i);
-		putchar(", ");
-	
+
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+
+	putchar('\n');
 	return (0);
 }
