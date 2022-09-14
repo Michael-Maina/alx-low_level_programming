81e38ce1bf76a753d0da83f8a20e83ba34ddeba9
rog-pog <michaelmaina02@gmail.com>
2022-09-14 01:41:45 -0700

    6-abs.c added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @a: integer to be checked
+ * Return: prints integer without a sign
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+		_putchar(a);
+	else
+		_putchar(a * -1);
+}
