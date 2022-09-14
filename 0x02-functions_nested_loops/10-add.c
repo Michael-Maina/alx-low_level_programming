5e360ee15b0f9e8cf6a4437d45bfd6ba0d382c92
rog-pog <michaelmaina02@gmail.com>
2022-09-14 06:58:20 -0700

    10-add.c added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and prints the result
+ * @a, b: integers to be added
+ *
+ * Return: prints sum; 0 otherwise
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	_putchar(sum + '0');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,13 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, solution;
 
 	for (i = 0; i <= 9; i++)
 	{	for (j = 0; j <= 9; j++)
 		{
-			_putchar(i * j);
+			solution = i * j;
+			_putchar(solution + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
