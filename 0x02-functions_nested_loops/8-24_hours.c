71ccab939470e4122ebcb1e54c4ec19c8a3b7064
rog-pog <michaelmaina02@gmail.com>
2022-09-14 04:50:53 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,27 +9,27 @@
 
 void jack_bauer(void)
 {
-	int i, j, k, l;
+int i, j, k, l;
 
-	for (i = 48; i <= 50; i++)
+for (i = 48; i <= 50; i++)
+{
+	for (j = 48; j <= 57; j++)
 	{
-		for (j = 48; j <= 57; j++)
-		{
-			if ((i <= 49 && j <= 57) || (i <= 50 && j <= 51))
+		if ((i <= 49 && j <= 57) || (i <= 50 && j <= 51))
+			{
+				for (k = 48; k <= 53; k++)
 				{
-					for (k = 48; k <= 53; k++)
+					for (l = 48; l <= 57; l++)
 					{
-						for (l = 48; l <= 57; l++)
-						{
-							_putchar(i);
-							_putchar(j);
-							_putchar(':');
-							_putchar(k);
-							_putchar(l);
-							_putchar('\n');
-						}
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(k);
+						_putchar(l);
+						_putchar('\n');
 					}
+				}
 			}
-		}
 	}
+}
 }
