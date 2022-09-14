29dc960d3bdd0a6dd80d3e7bbe60c598da8d8dfe
rog-pog <michaelmaina02@gmail.com>
2022-09-14 16:24:17 -0700

    103-fibonacci.c added

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,18 +12,20 @@ int main(void)
 	long int a = 1;
 	long int b = 2;
 	long int c;
-	long int sum = 0;
+	long int sum = b;
 
-        while (a + b < 4000000)
+	while (a + b < 4000000)
 	{
 		c = a + b;
-		a = b;
-		b = c;
 
 		if (c % 2 == 0)
 			sum = sum + c;
+
+		a = b;
+		b = c;
+
 		n++;
-	 }
+	}
 
 	printf("%ld\n", sum);
 	return (0);
