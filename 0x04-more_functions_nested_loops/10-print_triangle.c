89e6dab0e782b072b33ee9544d6857aa1e8bb194
rog-pog <michaelmaina02@gmail.com>
2022-09-15 15:46:09 -0700

    10-print_triangle.c added

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,9 +17,9 @@ void print_triangle(int size)
 
 	else
 	{
-		for (i = 1; i <= (size - 1); i++)
+		for (i = 0; i <= (size - 1); i++)
 		{
-			for (j = 1; j <= (size - 1) - i; j++)
+			for (j = 0; j <= (size - 1) - i; j++)
 				_putchar(' ');
 
 			for (k = 1; k <= i; k++)
