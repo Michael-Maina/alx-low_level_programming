f759be2d8766823a2c5955a348c3321720a0f75b
rog-pog <michaelmaina02@gmail.com>
2022-09-15 06:29:20 -0700

    5-more_numbers.c added

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,18 +9,20 @@
 
 void more_numbers(void)
 {
-	int i, j, k;
+        int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i >= 10)
+		for (j = 0; j <= 14; j++)
 		{
-			j = i % 10;
-			k = (i - j) / 10;
-			_putchar(k + '0');
-			_putchar(j + '0');
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+
+			_putchar((j % 10) + '0');
 		}
-		else
-			_putchar(i + '\n');
+
+		_putchar('\n');
 	}
 }
---
+++ 0x04-more_functions_nested_loops/trial.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+
+int main(void)
+{
+	more_numbers();
+
+	return (0);
+}
