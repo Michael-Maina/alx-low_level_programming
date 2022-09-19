5db8a4119be48dcf2edb2c685e2e6119211e1320
rog-pog <michaelmaina02@gmail.com>
2022-09-19 15:51:42 +0300

    1-swap.c added

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,6 +9,9 @@
 
 void swap_int(int *a, int *b)
 {
-	*a = b;
-	*b = a;
+	int temp; /*temporary variable to contain value of 1 pointer */
+
+	temp = *a; /*value of *a saved in temp */
+	*a = *b;   /*value of *b saved in *a */
+	*b = temp; /*value of temp(initial value of *a) now saved in *b */
 }
