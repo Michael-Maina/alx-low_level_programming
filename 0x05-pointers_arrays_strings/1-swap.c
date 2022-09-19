0f69328278372e3d0402ef610f85f674d9b03b7a
rog-pog <michaelmaina02@gmail.com>
2022-09-19 15:31:54 +0300

    1-swap.c added

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to be swapped with b
+ * @b: integer to be swapped with a
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
