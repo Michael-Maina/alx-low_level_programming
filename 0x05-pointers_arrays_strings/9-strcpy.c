2ddb13c54a82808000358a40e7822efa115fbc53
rog-pog <michaelmaina02@gmail.com>
2022-09-20 01:42:48 +0300

    9-strcpy.c added

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,10 +12,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int a, b = 0;
 
-	for (a = 0; src[a] != '\0'; ++a)
+	for (a = 0; src[a] != '\0'; a++)
 	{
 		dest[b] = src[a];
-		++b;
+		b++;
 	}
 	dest[b] = '\0';
 
