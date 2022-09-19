6767a08b9262b056ef3a58a191a0123529b97718
rog-pog <michaelmaina02@gmail.com>
2022-09-19 22:25:12 +0300

    5-rev_string.c added

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+int _strlen(char *s);
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: nothing
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int len;
+	char **rev_str = &s;
+
+	len = _strlen(s);
+
+	while (len >= i)
+	{
+		len--;
+		**(rev_str + i) = (*(s + len));
+		i++;
+	}
+}
+
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string whose length is to be determined
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; *s != '\0'; s++) /*counts characters in string until \0. */
+		a++;
+
+	return (a);
+}
