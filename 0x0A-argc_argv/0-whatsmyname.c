43827342b14913f7c77e33e7b41e3c37df8a34d8
Michael-Maina <michaelmaina02@gmail.com>
2022-09-30 11:06:22 +0300

    0-whatsmyname.c added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints name of program followed by a new line
+ *
+ * Return: nothing
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argc;
+	printf("%s\n",argv[0]);
+	return (0);
+}
