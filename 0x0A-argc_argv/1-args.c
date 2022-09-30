697991294ab8024d372183d10f0929c9ce7bba5c
Michael-Maina <michaelmaina02@gmail.com>
2022-09-30 11:18:36 +0300

    1-args.c added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it
+ *
+ * Return: nothing
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
