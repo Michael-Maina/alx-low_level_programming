878401649cc628c20807f5938afe80134934b6f9
Michael-Maina <michaelmaina02@gmail.com>
2022-10-11 22:59:29 +0300

    2-print_dog.c created

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints struct dog with the elements initialized
+ *           - if an element is empty, print (nil)
+ *           - if d is NULL, print nothing
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d);
+{
+	if (d == NULL)
+		return;
+
+	if (d->name)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil\n)");
+	if (d->age)
+		printf("Age: %.6f\n", d->age);
+	else
+		printf("Age: (nil\n)");
+	if (d->owner)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
+}
