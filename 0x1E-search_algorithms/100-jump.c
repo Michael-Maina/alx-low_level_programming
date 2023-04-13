7171a5fab02df089ae43a4f3a716df9dfad339b2
Michael-Maina <michaelmaina02@gmail.com>
2023-04-13 15:52:50 +0300

    Bug fixing

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,7 +13,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t i = 0;
 	size_t step, top;
 
-	if (array == NULL || size = 0)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	step = (sqrt(size));
